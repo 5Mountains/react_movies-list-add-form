68a55f7236843cf549257c42cb4c5e21fb6b7812
5Mountains <V.Piatyhor@gmail.com>
2021-01-25 20:20:56 +0200

    Updates

---
+++ precache-manifest.818a53f456cceb7fc84e0fd2fed299b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b5af99dc9c0a3287c3c0a6daf0af31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc5452987d71804ce53f",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "407c4a429ff6b6f70c5f",
+    "url": "./static/css/main.06aa85c7.chunk.css"
+  },
+  {
+    "revision": "cc5452987d71804ce53f",
+    "url": "./static/js/2.4769fc62.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.4769fc62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407c4a429ff6b6f70c5f",
+    "url": "./static/js/main.6bb7fda9.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.818a53f456cceb7fc84e0fd2fed299b4.js"
 );
 
 self.addEventListener('message', (event) => {
